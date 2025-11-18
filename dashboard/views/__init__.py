ab7ee4c552b7cf98bec65a676da5784ad276d26e
Snowblind5 <j4026617@gmail.com>
2025-11-18 11:16:04 +0100

    added methodology page

--- dashboard/views/__init__.py
+++ dashboard/views/__init__.py
@@ -1,4 +1,4 @@
-"""Dashboard pages package"""
-from . import overview, pool_explorer, risk_analysis, historical_trends
+"""Dashboard views package"""
+from . import overview, pool_explorer, risk_analysis, historical_trends, methodology
 
-__all__ = ['overview', 'pool_explorer', 'risk_analysis', 'historical_trends']
+__all__ = ['overview', 'pool_explorer', 'risk_analysis', 'historical_trends', 'methodology']
